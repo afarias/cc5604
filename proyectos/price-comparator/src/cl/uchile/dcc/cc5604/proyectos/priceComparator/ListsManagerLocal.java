cabd4bc78e93e8ae330c497aa114eba8d297a16e
Andres Farias <andres.farias@blueprintsit.cl>
2015-12-04 10:13:33 -0300

    Implementa interfaz.

--- proyectos/price-comparator/src/cl/uchile/dcc/cc5604/proyectos/priceComparator/ListsManagerLocal.java
+++ proyectos/price-comparator/src/cl/uchile/dcc/cc5604/proyectos/priceComparator/ListsManagerLocal.java
@@ -3,5 +3,5 @@ package cl.uchile.dcc.cc5604.proyectos.priceComparator;
 import javax.ejb.Local;
 
 @Local
-public interface ListsManagerLocal {
+public interface ListsManagerLocal extends IListManager {
 }
